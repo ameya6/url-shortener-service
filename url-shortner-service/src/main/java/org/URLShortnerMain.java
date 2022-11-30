7d59336437d58d7dff80345c92099712af888c3d
Ameya <ameyasawant5@gmail.com>
2022-11-30 16:02:51 +0530

    Added minor changes

--- url-shortner-service/src/main/java/org/URLShortnerMain.java
+++ url-shortner-service/src/main/java/org/URLShortnerMain.java
@@ -1,11 +1,13 @@
 package org;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @ComponentScan({"org.shortner", "org.data"})
+@Log4j2
 public class URLShortnerMain {
     public static void main(String[] args) {
         SpringApplication.run(URLShortnerMain.class, args);
